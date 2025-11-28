0ffe48ae21943d23223338b228bd19c8389f8a21
liminge <liminge@andrew.cmu.edu>
2025-11-28 13:03:12 +0800

    fix build issues

--- apps/expo/app/_layout.tsx
+++ apps/expo/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from 'expo-router';
+// Work around Expo Router Android bug where LinkPreview context may be missing in dev.
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - not part of the public API but safe for this workaround.
+import { LinkPreviewContextProvider } from 'expo-router/build/link/preview/LinkPreviewContext';
+
+import '../polyfills';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <LinkPreviewContextProvider>
+      <Stack screenOptions={{ headerShown: false }} />
+    </LinkPreviewContextProvider>
+  );
 }
